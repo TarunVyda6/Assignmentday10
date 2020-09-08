936967ccec5110e92284ebb23bf8f6ddebf2fefc
TarunVyda6 <kamaltarun.rao6@gmail.com>
2020-09-08 10:40:43 +0530

    new feature of problem of tie is resolved

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -4,17 +4,20 @@
 count=10
 headsCount=0
 tailsCount=0
-while(( $headsCount<21 && $tailsCount<21 ))
-do
-	result=$(( RANDOM%2 ))
 
-	if(( $result == 0 ))
-	then
-		headsCount=$(( $headsCount+1 ))
-	else
-		tailsCount=$(( $tailsCount+1 ))
-	fi
-done
+	while(( $headsCount<21 && $tailsCount<21 ))
+	do
+		result=$(( RANDOM%2 ))
+
+		if(( $result == 0 ))
+		then
+			headsCount=$(( $headsCount+1 ))
+		else
+			tailsCount=$(( $tailsCount+1 ))
+		fi
+	done
+
+
 if(( $headsCount>$tailsCount ))
 then
 	finalResult=$(( $headsCount-$tailsCount ))
@@ -24,6 +27,26 @@ then
 	finalResult=$(( $tailsCount-$headsCount ))
 	echo "tails has won and won by : "$finalResult
 else
-	echo "its a tie"
-fi
 
+	while [ $(( $headCount-$tailCount ))!=2 && $(( $tailCount-$headCount ))!=2 ]
+	do
+		result=$(( RANDOM%2 ))
+
+                if(( $result == 0 ))
+                then
+                        headsCount=$(( $headsCount+1 ))
+                else
+                        tailsCount=$(( $tailsCount+1 ))
+                fi
+        done
+
+	if(( $headsCount>$tailsCount ))
+		then
+	        finalResult=$(( $headsCount-$tailsCount ))
+        	echo "heads has won and won by : "$finalResult
+	elif(( $tailsCount>$headsCount ))
+	then
+        	finalResult=$(( $tailsCount-$headsCount ))
+	        echo "tails has won and won by : "$finalResult
+	fi
+fi
