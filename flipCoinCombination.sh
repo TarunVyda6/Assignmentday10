cb110362ec0488ac2bcac6109c7482bf1dac7651
TarunVyda6 <kamaltarun.rao6@gmail.com>
2020-09-08 11:26:26 +0530

    adding singlet combination and percentage determination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,13 +1,29 @@
 #! /bin/bash
 # welcome to flip coin combination
 
-result=$(( RANDOM%2 ))
+count=0
+hCount=0
+tCount=0
+while(( $count<10 ))
+do
+	result=$(( RANDOM%2 ))
+	count=$(( $count+1 ))
+	if(( $result == 0 ))
+	then
+		hCount=$(( $hCount+1 ))
+	else
+		tCount=$(( $tCount+1 ))
+	fi
+done
 
-if(( $result==0 ))
-then
-	echo "heads has won"
-else
-	echo "tails has won"
-fi
+declare -A combination
 
+combination["head"]=$hCount
+combination["tail"]=$tCount
+
+hPercentage=`awk "BEGIN {print $hCount*100/$count}"`
+tPercentage=`awk "BEGIN {print $tCount*100/$count}"`
+
+echo "heads percentage is "$hPercentage
+echo "tails percentage is "$tPercentage
 
