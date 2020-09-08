0fd297877aeb8b967140c01a6040ba77102af37d
TarunVyda6 <kamaltarun.rao6@gmail.com>
2020-09-08 06:15:03 +0530

    adding inputs to ArithmeticComputations

--- ArithmeticComputation.sh
+++ ArithmeticComputation.sh
@@ -1,3 +1,13 @@
 #! /bin/bash
 
 #welcome to Arithmetic Computations and sorting
+
+echo "please enter the input for a"
+read a
+
+echo "please enter the input for b"
+read b
+
+echo "please enter the input for c"
+read c
+
